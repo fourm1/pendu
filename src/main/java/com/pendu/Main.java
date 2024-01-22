68a087dac263f25032c9004fa26325e9a3faf5f8
ademareschal <toto.63146@gmail.com>
2024-01-22 16:29:20 +0100

    antoine: Adding a file containing the words, and a feature which add these words in an array

--- src/main/java/com/pendu/Main.java
+++ src/main/java/com/pendu/Main.java
@@ -1,7 +1,32 @@
 package com.pendu;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         // Lancer le jeu du pendu ici !
+        String wordsPath = "/resource/words.txt";
+        List<String> wordsToFind = new ArrayList<>();
+        String eachLine;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(wordsPath))) {
+
+            while ((eachLine = reader.readLine()) != null) {
+                wordsToFind.add(eachLine);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Test du tableau
+        for (int i = 0; i < eachLine.length(); i++) {
+            System.out.println(eachLine);
+        }
     }
 }
