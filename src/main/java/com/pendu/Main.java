86373e9a24db2a982e31de6494152530bb1553cd
ademareschal <toto.63146@gmail.com>
2024-01-23 14:57:20 +0100

    antoine: Testing the reading of the txt file and the take of a random word

--- src/main/java/com/pendu/Main.java
+++ src/main/java/com/pendu/Main.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +15,7 @@ public class Main {
         String wordsPath = "src/main/java/com/pendu/resource/words.txt";
         List<String> wordsToFind = new ArrayList<>();
         String eachLine;
-
+        Random random = new Random();
         try (BufferedReader reader = new BufferedReader(new FileReader(wordsPath))) {
 
             while ((eachLine = reader.readLine()) != null) {
@@ -24,9 +26,23 @@ public class Main {
             throw new RuntimeException(e);
         }
 
+        int randomInt = random.nextInt(30);
+
         //Test du tableau
+        String testWordsToFind = wordsToFind.get(randomInt);
         for (String wordTest : wordsToFind) {
             System.out.println(wordTest);
         }
+
+        System.out.println("------");
+        System.out.println(testWordsToFind);
+        int count = 0;
+
+        for (int i = 0; i < testWordsToFind.length(); i++) {
+            if (testWordsToFind.charAt(i) != ' ') {
+                count++;
+            }
+        }
+        System.out.println("Nombre total de caractères du mot-mystère : " + count);
     }
 }
