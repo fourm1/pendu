02f27d2ede3d7f75ebe387b36768081962791fed
ademareschal <toto.63146@gmail.com>
2024-01-24 15:27:58 +0100

    antoine: Updating the list of words and finishing the base game

--- src/main/java/com/pendu/Main.java
+++ src/main/java/com/pendu/Main.java
@@ -1,3 +1,9 @@
+// Bug : la même lettre peut être appelée plusieurs fois et peut permettre de faire remporter la victoire
+
+// À implémenter :
+//         - si une même lettre est présente plusieurs fois, il faut l'activer plusieurs fois
+//         - appeler le mot caché à chaque fois, et à chaque découverte de lettre, la mettre plusieurs fois
+
 package com.pendu;
 
 import java.io.BufferedReader;
@@ -13,19 +19,60 @@ public class Main {
         // Lancer le jeu du pendu ici !
 
         Scanner clavier = new Scanner(System.in);
-        System.out.println("Bonjour ! Quel est ton prénom ?");
+        System.out.println("Quel est ton prénom ?");
         String prenom = clavier.nextLine();
         System.out.println("Bonjour " + prenom + " ! Enchanté !");
         System.out.println("------");
 
         String hiddenWord = getString();
-        System.out.println("Mot à trouver : " + "-".repeat(hiddenWord.length()));
+        System.out.println(" ");
+        System.out.println(hiddenWord);
+        String hyphen = "-".repeat(hiddenWord.length());
+        System.out.println("Mot à trouver : " + hyphen);
+        System.out.println(" ");
 
         int life = 10;
         int foundLetters = 0;
+        int remainingLetters;
 
         while (life > 0 && foundLetters < hiddenWord.length()) {
-            Scanner letterGuessed = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+
+            if (life > 1) {
+                System.out.println(life + " vies restantes !");
+            } else {
+                System.out.println(life + " vie restante !");
+            }
+
+            System.out.println("Entrez une lettre : ");
+            String letterGuessed = scanner.nextLine();
+
+            if (letterGuessed.matches("[a-z]")) {
+                int verificationGuessedLetter = hiddenWord.indexOf(letterGuessed);
+
+                if (verificationGuessedLetter == -1) {
+                    System.out.println("Perdu ! Malheureusement, " + letterGuessed + " n'est pas une bonne lettre !");
+                    life--;
+                } else {
+                    System.out.println("Bravo ! " + letterGuessed + " est une bonne lettre !");
+                    foundLetters++;
+                    remainingLetters = hiddenWord.length() - foundLetters;
+                    System.out.println("Il vous reste " + remainingLetters + " lettres à trouver !");
+                }
+
+            } else if (letterGuessed.matches("[A-Z]")) {
+                System.out.println("Il n'y a aucune lettre majuscule.");
+
+            } else {
+                System.out.println(letterGuessed + " n'est pas une lettre de l'alphabet latin.");
+            }
+        }
+
+        if (foundLetters == hiddenWord.length()) {
+            System.out.println("Bravo " + prenom + " tu as gagné !");
+        } else if (life == 0) {
+            System.out.println("Tu as perdu, " + prenom + ". Le mot mystère était " + hiddenWord + " !");
+            System.out.println("Tu feras mieux la prochaine fois.");
         }
     }
 
